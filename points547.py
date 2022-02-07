49f2bad4360152aee684f0dcc7e3b91a20a83809
Kayana Tyson <kjt31@duke.edu>
2022-02-07 11:08:43 -0500

    Added if statements to pass more tests

--- points547.py
+++ points547.py
@@ -1,5 +1,10 @@
 #This contains the point function
 def points(a,b,c):
-    slope=b[1]-a[1]/(b[0]-a[0])
-    y=(slope*(c-b[0]))+b[1]
+    if a[0]==b[0]:
+        y=2
+    elif a[1]==b[1]:
+        y=1
+    else:
+        slope=b[1]-a[1]/(b[0]-a[0])
+        y=(slope*(c-b[0]))+b[1]
     return y
